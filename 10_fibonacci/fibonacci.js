285d88a96d9eb8d46055246c42be6701d71b7450
Jarod <jjarod517@gmail.com>
2024-12-14 23:47:21 -0500

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num)
+    let check = false
+    let error = ""
+
+    if (num < 0){
+        error = "OOPS"
+        check = true
+     }
+
+   
+
+    if (num === 1 || num === 2){
+        return 1
+    }
+
+    num = (((1 + Math.sqrt(5)) ** num) - ((1 - Math.sqrt(5)) ** num)) / ((2**num) * Math.sqrt(5))
+
+    if (check === true){
+        return error
+    }else{
+        return Math.round(num)
+    }
+    
 
 };
 
