75700afc1e5d08ef53ff9ec2a0fcde6c2e23a953
Jarod <jjarod517@gmail.com>
2024-12-15 00:03:21 -0500

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let result = []
+    for (const book of books) {
+        result.push(book.title)
+    }
 
+    return result
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
